54e69c6de9997d2303df7ebae19b5b6b5bda64ae
OrelStudio <orel890x@gmail.com>
2024-11-03 12:24:55 +0200

    fixed uselocalstoragehook

--- src/app/hooks/useLocalStorage.ts
+++ src/app/hooks/useLocalStorage.ts
@@ -1,4 +1,5 @@
 import {useEffect, useCallback, useSyncExternalStore} from 'react'
+import useRender from './useRender'
 
 /**
  * @description Subscribes to local storage events related to a key
@@ -17,19 +18,13 @@ const subscribeToKey = (key: string, callback: () => void) => {
   return () => window.removeEventListener('storage', onStorageEvent)
 }
 
-const getLocalStorageItem = (key: string) => {
-  const item = localStorage.getItem(key)
-
-  if (item) {
-    return JSON.parse(item)
-  }
-
-  return undefined
-}
+const getLocalStorageItem = (key: string) => localStorage.getItem(key)
 
 const setLocalStorage = <T>(key: string, value: T) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
+
+const getServerSnapshot = () => null
 
 /**
  * @description Custom hook to use local storage as a state
@@ -41,11 +36,13 @@ const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => voi
   const getSnapshot = useCallback(() => getLocalStorageItem(key), [key])
   // curried function to subscribe to local storage events
   const subscribe = useCallback((callback: () => void) => subscribeToKey(key, callback), [key])
-  const snapshot = useSyncExternalStore<T>(subscribe, getSnapshot)
+  const snapshot = useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
+  const render = useRender()
 
   const setState = useCallback((value: T): void => {
-    const newValue = typeof value === 'function' ? value(snapshot) : value
+    const newValue = typeof value === 'function' ? value(JSON.parse(snapshot || '')) : value
     setLocalStorage(key, newValue)
+    render()
   }, [key, snapshot])
 
   useEffect(() => {
@@ -54,7 +51,7 @@ const useLocalStorage = <T>(key: string, initialValue: T): [T, (value: T) => voi
     }
   }, [key])
 
-  return [snapshot ? snapshot : initialValue, setState]
+  return [snapshot ? JSON.parse(snapshot) as T : initialValue, setState]
 }
 
 export default useLocalStorage
